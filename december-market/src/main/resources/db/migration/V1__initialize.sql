d2d55c819b1796268761a3f45623f1612bbfa781
BreevE <e.m.breev@gmail.com>
2020-01-11 23:35:33 +0300

    hw2

--- december-market/src/main/resources/db/migration/V1__initialize.sql
+++ december-market/src/main/resources/db/migration/V1__initialize.sql
@@ -55,8 +55,25 @@ VALUES
 (1, 2),
 (1, 3);
 
-drop table if exists orders cascade;
-create table orders (id bigserial, user_id bigint, price numeric(8, 2) not null, address varchar (255) not null, phone_number varchar(30) not null, primary key(id), constraint fk_user_id foreign key (user_id) references users (id));
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders (
+  id bigserial,
+  user_id bigint,
+  price numeric(8, 2) not null,
+  address varchar (255) not null,
+  phone_number varchar(30) not null,
+  primary key(id),
+  constraint fk_user_id foreign key (user_id) references users (id)
+);
 
-drop table if exists orders_items cascade;
-create table orders_items (id bigserial, order_id bigint, product_id bigint, quantity int, price numeric(8, 2), primary key(id), constraint fk_prod_id foreign key (product_id) references products (id), constraint fk_order_id foreign key (order_id) references orders (id));
+DROP TABLE IF EXISTS orders_items CASCADE;
+CREATE TABLE orders_items (
+  id bigserial,
+  order_id bigint,
+  product_id bigint,
+  quantity int,
+  price numeric(8, 2),
+  primary key(id),
+  constraint fk_prod_id foreign key (product_id) references products (id),
+  constraint fk_order_id foreign key (order_id) references orders (id)
+);
