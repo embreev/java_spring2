58a923e8be65a100a997f992b9a3c96b67eb67d6
e.m.breev <e.m.breev@gmail.com>
2020-01-26 11:54:08 +0300

    imported the project from the lesson

---
+++ december-market/src/main/java/com/geekbrains/decembermarket/entites/Product.java
@@ -0,0 +1,35 @@
+package com.geekbrains.decembermarket.entites;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product")
+    private List<Review> reviews;
+}
